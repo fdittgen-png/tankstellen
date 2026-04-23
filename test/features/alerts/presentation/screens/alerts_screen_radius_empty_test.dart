f042395a04c11e02358cf6c1ff32a4d26fafce6c
fdittgen <fdittgen@gmail.com>
2026-04-23 08:14:43 +0200

    fix(lint): drop unused flutter/widgets.dart import in alerts_screen_radius_empty_test

--- test/features/alerts/presentation/screens/alerts_screen_radius_empty_test.dart
+++ test/features/alerts/presentation/screens/alerts_screen_radius_empty_test.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/widgets.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:tankstellen/features/alerts/data/models/price_alert.dart';
